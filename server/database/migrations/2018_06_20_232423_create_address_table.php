5189f34ab105f7f0c14366a38eac8e014f2a6818
Ryan Hein <admin@heimech.com>
2018-06-20 17:28:08 -0600

    Added migrations

---
+++ server/database/migrations/2018_06_20_232423_create_address_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('address', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->string('street', 255);
+            $table->string('city', 255);
+            $table->string('state', 255);
+            $table->string('postal_code', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ server/database/migrations/2018_06_20_232534_create_work_connection_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorkConnectionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('work_connection', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('person_id')->unsigned();
+            $table->integer('colleague_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
