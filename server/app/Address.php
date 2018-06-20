c2fd6ed8ebf411fb99d5a4570ef0311543fac6f1
Ryan Hein <admin@heimech.com>
2018-06-20 17:36:20 -0600

    Added Eloquent Relationships for Address Model

--- server/app/Address.php
+++ server/app/Address.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    //
+    /**
+     * An address belongs to a person
+     */
+    public function person()
+    {
+        return $this->belongsTo(Person::class);
+    }
 }
