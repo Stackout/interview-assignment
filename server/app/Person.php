36070c8749ccadc9184d4c2122577015439b2302
Ryan Hein <admin@heimech.com>
2018-06-20 17:35:23 -0600

    Added Eloquent Relationships

--- server/app/Person.php
+++ server/app/Person.php
@@ -4,7 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Person extends Model
 {
-    //
+    /**
+     * All people have many colleague connections.
+     */
+    public function colleagues(){
+
+        return $this->belongsToMany(Person::class, 'work_connection', 'person_id', 'colleague_id');
+
+    }
+
+    /**
+     * All People have an address
+     */
+    public function address()
+    {
+        return $this->hasOne(Address::class);
+    }
 }
